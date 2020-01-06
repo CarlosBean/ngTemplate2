4f64005fc426badd87891a8413646c8407e86dd4
Carlos Benavides <1992cab@gmail.com>
2020-01-06 00:51:37 -0500

    chore: added icons

--- src/assets/icons/font-awesome-icons.ts
+++ src/assets/icons/font-awesome-icons.ts
@@ -1,7 +1,21 @@
 import {
   faUser,
   faPowerOff,
-  faAngleRight
+  faAngleRight,
+  faTimes,
+  faCog,
+  faPen,
+  faSearch,
+  faPlusCircle
 } from '@fortawesome/free-solid-svg-icons';
 
-export const fontAwesomeIcons = [faUser, faPowerOff, faAngleRight];
+export const fontAwesomeIcons = [
+  faUser,
+  faPowerOff,
+  faAngleRight,
+  faTimes,
+  faCog,
+  faPen,
+  faSearch,
+  faPlusCircle
+];
